8608f647da6017bac0bc281c65c7272e0da83e57
ErikRSN <driedgrapes506@gmail.com>
2024-11-05 07:59:36 +0100

    Refactor feature calculations and clean up commented code in main.py

--- main.py
+++ main.py
@@ -60,44 +60,47 @@ df.columns = df.columns.str.lower()
 
 df
 
-
-### Calculate features and technical indicators for each stock.
-
-df['garman_klass_vol'] = ((np.log(df['high'])-np.log(df['low']))**2)/2-(2*np.log(2)-1)*((np.log(df['adj close'])-np.log(df['open']))**2)
-
-df['rsi'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.rsi(close=x, length=20))
-
-df['bb_low'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:,0])
-                                                          
-df['bb_mid'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:,1])
-                                                          
-df['bb_high'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:,2])
-
-def compute_atr(stock_data):
-    atr = pandas_ta.atr(high=stock_data['high'],
-                        low=stock_data['low'],
-                        close=stock_data['close'],
-                        length=14)
-    return atr.sub(atr.mean()).div(atr.std())
-
-df['atr'] = df.groupby(level=1, group_keys=False).apply(compute_atr)
-
-def compute_macd(close):
-    macd = pandas_ta.macd(close=close, length=20).iloc[:,0]
-    return macd.sub(macd.mean()).div(macd.std())
-
-df['macd'] = df.groupby(level=1, group_keys=False)['adj close'].apply(compute_macd)
-
-df['dollar_volume'] = (df['adj close']*df['volume'])/1e6
-
-df
-
-
 print(df.head())
 
 
-# Aggregate to monthly level and filter top 150 most liquid stocks for each month.
-# To reduce training time and experiment with features and strategies, we convert the business-daily data to month-end frequency.
+
+### Calculate features and technical indicators for each stock.
+
+# df['garman_klass_vol'] = ((np.log(df['high'])-np.log(df['low']))**2)/2-(2*np.log(2)-1)*((np.log(df['adj close'])-np.log(df['open']))**2)
+
+# df['rsi'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.rsi(close=x, length=20))
+
+# df['bb_low'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:,0])
+                                                          
+# df['bb_mid'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:,1])
+                                                          
+# df['bb_high'] = df.groupby(level=1)['adj close'].transform(lambda x: pandas_ta.bbands(close=np.log1p(x), length=20).iloc[:,2])
+
+# def compute_atr(stock_data):
+#     atr = pandas_ta.atr(high=stock_data['high'],
+#                         low=stock_data['low'],
+#                         close=stock_data['close'],
+#                         length=14)
+#     return atr.sub(atr.mean()).div(atr.std())
+
+# df['atr'] = df.groupby(level=1, group_keys=False).apply(compute_atr)
+
+# def compute_macd(close):
+#     macd = pandas_ta.macd(close=close, length=20).iloc[:,0]
+#     return macd.sub(macd.mean()).div(macd.std())
+
+# df['macd'] = df.groupby(level=1, group_keys=False)['adj close'].apply(compute_macd)
+
+# df['dollar_volume'] = (df['adj close']*df['volume'])/1e6
+
+# df
+
+
+# print(df.head())
+
+
+# # Aggregate to monthly level and filter top 150 most liquid stocks for each month.
+# # To reduce training time and experiment with features and strategies, we convert the business-daily data to month-end frequency.
 
 
 last_cols = [c for c in df.columns.unique(0) if c not in ['dollar_volume', 'volume', 'open',
@@ -110,38 +113,38 @@ data = (pd.concat([df.unstack('ticker')['dollar_volume'].resample('M').mean().st
 data
 
 
-# Calculate 5-year rolling average of dollar volume for each stocks before filtering.
+# # Calculate 5-year rolling average of dollar volume for each stocks before filtering.
 
-data['dollar_volume'] = (data.loc[:, 'dollar_volume'].unstack('ticker').rolling(5*12, min_periods=12).mean().stack())
+# data['dollar_volume'] = (data.loc[:, 'dollar_volume'].unstack('ticker').rolling(5*12, min_periods=12).mean().stack())
 
-data['dollar_vol_rank'] = (data.groupby('date')['dollar_volume'].rank(ascending=False))
+# data['dollar_vol_rank'] = (data.groupby('date')['dollar_volume'].rank(ascending=False))
 
-# data = data[data['dollar_vol_rank']<150].drop(['dollar_volume', 'dollar_vol_rank'], axis=1)
+# # data = data[data['dollar_vol_rank']<150].drop(['dollar_volume', 'dollar_vol_rank'], axis=1)
 
-# data
-
-
-# Calculate Monthly Returns for different time horizons as features.
-
-# To capture time series dynamics that reflect, for example, momentum patterns, we compute historical returns using the method .pct_change(lag), that is, returns over various monthly periods as identified by lags.
+# # data
 
 
-def calculate_returns(df):
+# # Calculate Monthly Returns for different time horizons as features.
 
-    outlier_cutoff = 0.005
+# # To capture time series dynamics that reflect, for example, momentum patterns, we compute historical returns using the method .pct_change(lag), that is, returns over various monthly periods as identified by lags.
 
-    lags = [1, 2, 3, 6, 9, 12]
 
-    for lag in lags:
+# def calculate_returns(df):
 
-        df[f'return_{lag}m'] = (df['adj close']
-                              .pct_change(lag)
-                              .pipe(lambda x: x.clip(lower=x.quantile(outlier_cutoff),
-                                                     upper=x.quantile(1-outlier_cutoff)))
-                              .add(1)
-                              .pow(1/lag)
-                              .sub(1))
-    return df
+#     outlier_cutoff = 0.005
+
+#     lags = [1, 2, 3, 6, 9, 12]
+
+#     for lag in lags:
+
+#         df[f'return_{lag}m'] = (df['adj close']
+#                               .pct_change(lag)
+#                               .pipe(lambda x: x.clip(lower=x.quantile(outlier_cutoff),
+#                                                      upper=x.quantile(1-outlier_cutoff)))
+#                               .add(1)
+#                               .pow(1/lag)
+#                               .sub(1))
+#     return df
     
     
 # data = data.groupby(level=1, group_keys=False).apply(calculate_returns).dropna()
@@ -149,25 +152,37 @@ def calculate_returns(df):
 # data
 
 
+# # Download Fama-French Factors and Calculate Rolling Factor Betas.
+
+# factor_data = web.DataReader('F-F_Research_Data_5_Factors_2x3',
+#                                'famafrench',
+#                                start='2010')[0].drop('RF', axis=1)
+
+# factor_data.index = factor_da ta.index.to_timestamp()
+
+# factor_data = factor_data.resample('M').last().div(100)
+
+# factor_data.index.name = 'date'
+
+# factor_data = factor_data.join(data['return_1m']).sort_index()
+
+# factor_data
 
 
-def calculate_returns(df):
 
-    outlier_cutoff = 0.005
+# # Ensure both indices are timezone-naive
+# factor_data.index = factor_data.index.tz_localize(None)
+# data.index = data.index.tz_localize(None)
 
-    lags = [1, 2, 3, 6, 9, 12]
+# # Join the data DataFrame with factor_data
+# factor_data = factor_data.join(data['return_1m']).sort_index()
 
-    for lag in lags:
+# # Display the first few rows of the factor_data DataFrame
+# print(factor_data.head())
 
-        df[f'return_{lag}m'] = (df['adj close']
-                              .pct_change(lag)
-                              .pipe(lambda x: x.clip(lower=x.quantile(outlier_cutoff),
-                                                     upper=x.quantile(1-outlier_cutoff)))
-                              .add(1)
-                              .pow(1/lag)
-                              .sub(1))
-    return df
-    
+
+
+
 
 
 # # Filter the DataFrame for Apple stock
@@ -192,3 +207,15 @@ def calculate_returns(df):
 # plt.ylabel('Volatility')
 # plt.legend()
 # plt.show()
+
+
+
+from sklearn.cluster import KMeans
+
+def get_clusters(df): 
+    df['cluster'] = KMeans(n_clusters=5, random_state=0).fit(df[['return_1m', 'return_2m', 'return_3m', 'return_6m', 'return_9m', 'return_12m']]).labels_
+    return df
+
+# data = data.dropna().groupby(level=1, group_keys=False).apply(get_clusters)
+
+# data
